b18c93d40133086bc2e3a824ee7f6db42e14dd24
Cherish Clark <cherish.g.clark@gmail.com>
2017-12-09 16:10:13 -0700

    Integreated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,4 +41,11 @@ puts "10 blog posts created"
   end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "technolgy #{technology}"
+    )
+end
+puts "3 technologies created"
 end
